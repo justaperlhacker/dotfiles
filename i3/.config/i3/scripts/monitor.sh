9c14ba7f68a292ab9866075da02049fa2871c47d
satrac <satrac@gmail.com>
2026-08-15 23:17:57 -0400

    updates

--- i3/.config/i3/scripts/monitor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-/usr/bin/xrandr \
-  --output HDMI-A-1 --mode 1920x1080 --pos 0x0 --rotate normal --primary \
-  --output DisplayPort-3 --mode 1920x1080 --pos 1920x0 --rotate normal \
-  --output DisplayPort-4 --off \
-  --output DisplayPort-5 --off
